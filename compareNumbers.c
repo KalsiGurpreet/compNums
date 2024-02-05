b59aacd638b9d4f02aaacdb2afbb699954e00cdf
Gurpreet Kalsi <gups.kalsi@gmail.com>
2024-02-05 12:55:33 -0400

    added some information in comments of a program

--- compareNumbers.c
+++ compareNumbers.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-    puts("Enter two integers and I will tell you");
-    printf("%s", "the relationship %s between them:\n");
-
+    puts("Enter two integers and I will tell you");   // display message on screen.
+    printf("%s", "the relationship between them:\n"); // \ backslash is an escape character and \n is
+                                                      // called escape sequence and it means new line.
+                                                      // scanf and printf are not part of C programming.
+    
     int int1 =0;
     int int2 =0;
 
@@ -36,4 +38,17 @@ int main(void)
 	printf("%d is less than or equal to %d.\n", int1, int2);
     }
     return 0;
+
+/* Standard library functions like printf and scanf are not part of the C programming
+language. For example, the compiler cannot find a spelling error in printf or scanf.
+When compiling a printf statement, the compiler merely provides space in the
+object program for a “call” to the library function. But the compiler does not know
+where the library functions are—the linker does. When the linker runs, it locates the
+library functions and inserts the proper calls to these functions in the object program.
+Now the object program is complete and ready to execute. The linked program is
+called an executable. If the function name is misspelled, the linker will spot the
+error—it will not be able to match the name in the program with the name of any
+known function in the libraries. */
+    
 }
+
